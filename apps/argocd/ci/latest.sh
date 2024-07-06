f010b84c882fdec601d15fb6fe4f45d2b0d4b70b
VirtualDisk <zoe@zoeblan.co>
2024-07-06 01:18:54 -0400

    temporary argocd version override

--- apps/argocd/ci/latest.sh
+++ apps/argocd/ci/latest.sh
@@ -2,4 +2,5 @@
 version=$(curl -sX GET "https://api.github.com/repos/argoproj/argo-cd/releases/latest" | jq --raw-output '.tag_name')
 version="${version#*v}"
 version="${version#*release-}"
-printf "%s" "${version}"
+# printf "%s" "${version}"
+printf "%s" "2.11.4"
