3a62b9a6ff601bbcc295f669af9b13a2ce3c9f44
VirtualDisk <zoe@zoeblan.co>
2024-07-13 16:12:59 -0400

    fix versioning for talos-node-mgmt

--- apps/talos-node-mgmt/ci/latest.sh
+++ apps/talos-node-mgmt/ci/latest.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/rancher/system-upgrade-controller/releases/latest" | jq --raw-output '.tag_name')
+version=$(curl -sX GET "https://api.github.com/repos/siderolabs/talos/releases/latest" | jq --raw-output '.tag_name')
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
