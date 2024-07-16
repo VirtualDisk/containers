29a7cb08493cde5ed86db3917438b491cc64a898
Zoë B <zoe.blanco1911@gmail.com>
2024-07-16 13:46:51 -0400

    Update latest.sh

--- apps/argocd/ci/latest.sh
+++ apps/argocd/ci/latest.sh
@@ -2,5 +2,4 @@
 version=$(curl -sX GET "https://api.github.com/repos/argoproj/argo-cd/releases/latest" | jq --raw-output '.tag_name')
 version="${version#*v}"
 version="${version#*release-}"
-# printf "%s" "${version}"
-printf "%s" "2.11.5"
+printf "%s" "${version}"
