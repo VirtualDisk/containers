82d08767070379830b5435c0e5983f5fb0bbe235
VirtualDisk <zoe@zoeblan.co>
2025-02-11 15:01:31 -0500

    fixes

--- apps/git-to-ollama/entrypoint.sh
+++ apps/git-to-ollama/entrypoint.sh
@@ -2,22 +2,20 @@
 set -eo pipefail
 set -x
 
-git clone "${GIT_REPO}" /repo
-DOCUMENTS=$(find /repo)
+DOCUMENTS=$(find /src)
 for file in $DOCUMENTS; do
     echo "Uploading file ${file}"
     FILE_ID=$(curl -X POST \
         -H "Authorization: Bearer ${OLLAMA_API_KEY}" \
         -H "Accept: application/json" \
         -F "file=@$file" \
-        "https://open-webui.verde.zoe/api/v1/files/" |
-        jq -r .id)
+        "${OPEN_WEBUI_URL}/api/v1/files/" | jq -r '.id')
 
     echo "Adding file ${file} to knowledge base ${KNOWLEDGE_BASE_ID}"
     curl -X POST \
         -H "Authorization: Bearer ${OLLAMA_API_KEY}" \
         -H "Content-Type: application/json" \
-        -d '{"file_id:" "'"${FILE_ID}"'"}' \
-        "https://open-webui.verde.zoe/api/v1/knowledge/${KNOWLEDGE_BASE_ID}/file/add/"
+        -d '{"file_id": "'"${FILE_ID}"'"}' \
+        "${OPEN_WEBUI_URL}/api/v1/knowledge/${KNOWLEDGE_BASE_ID}/file/add"
     echo "Added file ${file} to knowledge base ${KNOWLEDGE_BASE_ID}."
 done
