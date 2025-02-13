cf66f3c5eb8362c1bec43e6ff422fa6d41fe603b
VirtualDisk <zoe@zoeblan.co>
2025-02-13 15:59:20 -0500

    new functions

--- apps/talos-node-mgmt/entrypoint.sh
+++ apps/talos-node-mgmt/entrypoint.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env sh
 
-talosctl --nodes=$NODE_IP reset --reboot="${REBOOT:-true}" --wait=false &&
-    kubectl delete node $NODE --wait=false
+health() {
+    until kubectl get volumes.longhorn.io -A | grep "degraded" || true; do
+        echo "Some Longhorn volumes are degraded. Trying again in ${RETRY_TIME:-5} seconds."
+        sleep "${RETRY_TIME:-5}"
+    done
+
+    talosctl --nodes=$NODE_IP health
+}
+
+reset() {
+    talosctl --nodes=$NODE_IP reset --reboot="${REBOOT:-true}" --wait=false &&
+        kubectl delete node $NODE --wait=false
+}
+
+"$@"
