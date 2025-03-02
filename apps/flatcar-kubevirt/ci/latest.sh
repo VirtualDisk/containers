49ecaa4d4c2ec36795593bf647b1573de94a56b8
VirtualDisk <zoe@zoeblan.co>
2025-03-02 16:29:03 -0500

    fix homeassistant and remove flatcar-kubevirt

--- apps/flatcar-kubevirt/ci/latest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-version=$(curl "https://stable.release.flatcar-linux.net/amd64-usr/current/version.txt" | grep FLATCAR_VERSION= | sed 's/FLATCAR_VERSION=//g')
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
