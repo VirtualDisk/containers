da18be190b44496e871c33ae378abb35b5f2f192
VirtualDisk <zoe@zoeblan.co>
2025-06-17 00:40:29 -0400

    remove unused images

--- apps/openwebrxplus/ci/latest.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-version=$(cat apps/openwebrxplus/Dockerfile | awk -F ':' '{print $2}' | awk -F '@' '{print $1}')
-printf "%s" "${version}"
--- apps/talos-image-factory/ci/latest.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-version=$(grep factory apps/talos-image-factory/Dockerfile | head -n 1 | awk -F ':' '{print $2}' | cut -d '@' -f 1)
-printf "%s" "${version}"
